cfc44b34ff3fea928d837016ee2bd3c05b6584c1
Panurat <paunavt5@gmail.com>
2024-07-06 14:04:05 +0700

    Add api bot

--- test/call_token.py
+++ test/call_token.py
@@ -1,7 +1,7 @@
 import requests
 
 # URL ของ Web App ที่คุณสร้างขึ้น
-url = 'https://script.google.com/macros/s/AKfycbwN5yqRe4uWwaEUtZ5emNhp4nr3Cwgh2J-Qn5mSiUDlr1wU3DlegiLfzHL0c0qeyFbN/exec'
+url = 'https://script.google.com/macros/s/AKfycbzgQ6SmGCYyuX_elxSP1IETzfCjhQ-zVFpl7nNcZXr2J7V_Ws52n6i2CjKWxncQ0QDV/exec'
 
 # ส่ง GET request เพื่อดึงข้อมูล
 response = requests.get(url)
@@ -10,22 +10,29 @@ response = requests.get(url)
 if response.status_code == 200:
     data = response.json()
 
-    # ดึงข้อมูลจาก JSON และแยกออกเป็นรายการแต่ละชนิดของคุกกี้
-    c_user = [item['c_user'] for item in data['data']]
-    xs = [item['xs'] for item in data['data']]
-    datr = [item['datr'] for item in data['data']]
-    fr = [item['fr'] for item in data['data']]
+    try:
+        # ดึงข้อมูลจาก JSON และแยกออกเป็นรายการแต่ละชนิดของคุกกี้
+        b_id = [item['b_id'] for item in data['data']]
+        c_user = [item['c_user'] for item in data['data']]
+        xs = [item['xs'] for item in data['data']]
+        datr = [item['datr'] for item in data['data']]
+        fr = [item['fr'] for item in data['data']]
 
-    # รวมข้อมูลเป็นรายการของคุกกี้
-    cookies = {
-        'c_user': c_user,
-        'xs': xs,
-        'datr': datr,
-        'fr': fr
-    }
+        # รวมข้อมูลเป็นรายการของคุกกี้
+        cookies = {
+            'b_id': b_id,
+            'c_user': c_user,
+            'xs': xs,
+            'datr': datr,
+            'fr': fr
+        }
+
+        # แสดงผลข้อมูล
+        for cookie_type, values in cookies.items():
+            print(f'{cookie_type}: {values}')
+
+    except KeyError as e:
+        print(f'เกิดข้อผิดพลาดในการดึงข้อมูล: {e}')
 
 else:
     print('เกิดข้อผิดพลาดในการเรียกใช้ API:', response.status_code)
-
-for cookie_type, values in cookies.items():
-    print(f'{cookie_type}: {values}')
