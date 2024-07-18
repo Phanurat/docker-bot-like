e727f50a741e35ca4bf37011cf645171ef34a8e5
Panurat <paunavt5@gmail.com>
2024-07-18 13:13:01 +0700

    date_set

---
+++ test/datetype.py
@@ -0,0 +1,40 @@
+import datetime
+
+# วันที่ปัจจุบัน
+today = datetime.date.today()
+
+# วันในสัปดาห์
+day_of_week = today.weekday()
+
+# แสดงวันในสัปดาห์
+days = ["จันทร์", "อังคาร", "พุธ", "พฤหัสบดี", "ศุกร์", "เสาร์", "อาทิตย์"]
+print(f"วันนี้เป็นวัน{days[day_of_week]}")
+
+
+if days == str(days[0]):
+    print(f"สวัสดีวัน {days[day_of_week]}")
+
+elif days == str(days[1]):
+    print(f"สวัสดีวัน {days[day_of_week]}")
+
+elif days == str(days[2]):
+    print(f"สวัสดีวัน {days[day_of_week]}")
+
+elif days == str(days[3]):
+    print(f"สวัสดีวัน {days[day_of_week]}")
+
+elif days == str(days[4]):
+    print(f"สวัสดีวัน {days[day_of_week]}")
+
+elif days == str(days[4]):
+    print(f"สวัสดีวัน {days[day_of_week]}")
+
+elif days == str(days[5]):
+    print(f"สวัสดีวัน {days[day_of_week]}")
+
+elif days == str(days[6]):
+    print(f"สวัสดีวัน {days[day_of_week]}")
+
+
+
+
