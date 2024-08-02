8e2691fd54410b922bd13761851e1bb51b5a6c20
Panurat <paunavt5@gmail.com>
2024-08-02 17:18:15 +0700

    update code automate file test -- test file

--- prototype/test_run copy.py
+++ prototype/test_run copy.py
@@ -1,60 +1,78 @@
-import requests
+from typing import KeysView
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime
 import time
 import random
+import requests
 
-target_b_id = 'b00009'
-
-def break_automate():
-    time_random = random.randint(5, 10)
-    print("Break time:", time_random, "seconds")
-    time.sleep(time_random)
-    
 def get_driver():
-    # Options for ChromeDriver
     chrome_options = Options()
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument("--disable-notifications")
 
-    # Initialize WebDriver using WebDriverManager
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     return driver
 
+# Input ID bot from API google sheet
+target_b_id = 'b00009'
+
+# Scroll post Facebook
+def timeline_scroll(driver):
+    scroll_random = random.uniform(4, 6)
+    print("Timeline Scroll Monitor!!")
+    for _ in range(int(scroll_random)):
+        driver.execute_script("window.scrollBy(0, 180);")
+        time.sleep(15)
+
+# Sleep Break Bot automation
+def break_time(driver):
+    start_time = 100  # 1 hour 40 minutes = 6000 sec
+    end_time = 200  # 3 hours 20 minutes = 12000 sec
+    break_duration = random.randint(start_time, end_time)
+    print(f"Time break is {break_duration} sec")
+    print("--------------------------------")
+    driver.quit()
+    time.sleep(break_duration)
+
 def check_login(driver):
-    url = 'https://script.google.com/macros/s/AKfycbxYQWVejdmhc3P99N0-qSgHDfcLX3PI1sQFJd2txN-eV0rKg0NqzF7tPBYjk1sGeAOz/exec'
-    response = requests.get(url)
+    print("Start Bot Running!!!")
+    url = 'https://www.facebook.com/'
+    driver.get(url)
 
-    if response.status_code == 200:
-        data = response.json()
-        try:
-            selected_data = [item for item in data['data'] if item['b_id'] == target_b_id]
+    # Print the title and URL
+    print("Title:", driver.title)
+    print("URL:", driver.current_url)
 
-            if not selected_data:
-                print(f"No data found for b_id = {target_b_id}")
-                return False
-
-            item = selected_data[0]
-            c_user = item['c_user']
-            xs = item['xs']
-            datr = item['datr']
-            fr = item['fr']
-
-        except KeyError as e:
-            print(f'Error extracting data: {e}')
-            return False
-
-    else:
-        print('Error calling API:', response.status_code)
+    bot_url = 'https://script.google.com/macros/s/AKfycbxYQWVejdmhc3P99N0-qSgHDfcLX3PI1sQFJd2txN-eV0rKg0NqzF7tPBYjk1sGeAOz/exec'
+    try:
+        response = requests.get(bot_url)
+        response.raise_for_status()  # Raise an HTTPError for bad responses
+    except requests.RequestException as e:
+        print(f"Error calling API: {e}")
         return False
 
-    url = 'https://www.facebook.com'
-    driver.get(url)
+    data = response.json()
+    try:
+        selected_data = [item for item in data['data'] if item['b_id'] == target_b_id]
+        if not selected_data:
+            print(f"No data found for b_id = {target_b_id}")
+            return False
+        item = selected_data[0]
+        c_user = item['c_user']
+        xs = item['xs']
+        datr = item['datr']
+        fr = item['fr']
+    except KeyError as e:
+        print(f"Error extracting data: {e}")
+        return False
 
     cookies_list = [
         {
@@ -122,16 +140,24 @@ def main():
     if check_login(driver):
         count = 1
         while True:
-            work_time = random.randint(5, 10)
-            print("Automation time:", work_time, "seconds")
-            time.sleep(work_time)  # Wait for random time
+            start_time = time.time()
+            two_hour_time = 72  # 2 hours
+            three_hour_time = 120  # 3.5 hours
+            work_time = random.randint(two_hour_time, three_hour_time)
+            print(f"Time to Work! {work_time} sec.")
+            
+            while time.time() - start_time < work_time:
+                timeline_scroll(driver)
+                time.sleep(3)
+
+            break_time(driver)
 
             count += 1
             if count % 10 == 0:
-                driver.quit()  # Quit the current driver
-                break_automate()
                 driver = get_driver()  # Restart the driver
-                check_login(driver)
+                if not check_login(driver):
+                    print("Login failed after restart.")
+                    break
             time.sleep(1)
 
 if __name__ == "__main__":
